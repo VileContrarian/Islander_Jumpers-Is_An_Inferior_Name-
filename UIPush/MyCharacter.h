777a72648956d04aaf9ea07aafaa64447098bf41
Frost552 <37560393+Frost552@users.noreply.github.com>
2019-02-07 17:54:33 -0500

    First UI Push!
    Rename source and header files as Nessecary, Character should be whatever the Gameplay guys push, this is my temp file to hold variables to load a health bar
    
    Probably mostly unusable now because there is nothing to hook up to anything else, but given the right assets this should work with the player classes
    
    Ill sort these out for the next push

---
+++ UIPush/MyCharacter.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class TWISTEDMARIO_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+	
+	UPROPERTY(BlueprintReadWrite, Category = "UI") //brings the players health into the blueprint
+		float playerHealth = 50.0f; //Player health, set to 50 to show health bar change
+	UPROPERTY(BlueprintReadWrite, Category = "UI")//brings the players health percentage into the blueprint
+		float playerHealthPercent; //player health percentage
+	float maxHP = 100.0f; //players max HP
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
