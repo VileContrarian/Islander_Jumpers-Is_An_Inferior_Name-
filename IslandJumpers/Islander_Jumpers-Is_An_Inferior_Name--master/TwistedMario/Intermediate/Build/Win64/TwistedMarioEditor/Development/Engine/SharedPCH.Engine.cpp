b867e48255eaa56386169b26cae4873ab8357ebb
Frost552 <37560393+Frost552@users.noreply.github.com>
2019-03-14 15:08:35 -0400

    Big UI Push
    Assets
    Menus
    navigation

--- IslandJumpers/Islander_Jumpers-Is_An_Inferior_Name--master/TwistedMario/Intermediate/Build/Win64/TwistedMarioEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Brand/Desktop/IslandJumpers/Islander_Jumpers-Is_An_Inferior_Name--master/TwistedMario/Intermediate/Build/Win64/TwistedMarioEditor/Development/Engine/SharedPCH.Engine.h"
--- UI 3.5.2019/Intermediate/Build/Win64/TwistedMarioEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Brand/Desktop/UICrew/UIDeligates/TwistedMario/Intermediate/Build/Win64/TwistedMarioEditor/Development/Engine/SharedPCH.Engine.h"
