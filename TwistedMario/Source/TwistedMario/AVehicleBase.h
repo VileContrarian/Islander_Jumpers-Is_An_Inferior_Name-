fa8dbc1df1e993a09190ef72ae2d759613e2b342
William Ch <wchristan@gmail.com>
2019-03-17 21:27:22 -0400

    Vehicle Updates
    Moving & Turning
    1. Can't turn when not moving
    2. Can't accelerate in the air
    3. Added acceleration rate & decay
    4. Added condition in the Hover Component to dampen near instant upon landing
    5. There's more but I can't remember....

--- TwistedMario/Source/TwistedMario/AVehicleBase.h
+++ TwistedMario/Source/TwistedMario/AVehicleBase.h
@@ -3,78 +3,257 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "WheeledVehicle.h"
-#include "Components/InputComponent.h"
+#include "GameFramework/Pawn.h"
 #include "AVehicleBase.generated.h"
 
-/**
+/*
  *
  */
 UCLASS()
-class TWISTEDMARIO_API AVehicleBase : public AWheeledVehicle
+class TWISTEDMARIO_API AVehicleBase : public APawn
 {
 	GENERATED_BODY()
+
 public:
 	// Sets default values for this pawn's properties
-	//UE has its own garbage collection system, so no ~() destructor
-	//This is the constructor
 	AVehicleBase();
 
-	//Public Variables
-	UPROPERTY(EditAnywhere, Category = "Vehicles")//This will make it editable on the editor (i.e. Unity's inspector)
-		int health; //Current health
-	UPROPERTY(EditAnywhere, Category = "Vehicles")
-		int attack; //Vehicle strength
-	uint8 lap; //Keep track of laps completed
-
-	//Public Methods
-	virtual void Accelerate();
-	virtual void Reverse();
-	virtual void Drift();
-	virtual void Jump();
-	virtual void UseItem();
-	virtual void CurrentItem();
-	virtual void GetVelocity(FVector vec);
-	virtual void Damage(int value);
-	virtual void Update();
-	virtual void HandleEvents();
-
-	//Adding Transform component
-	UPROPERTY()
-		USceneComponent* Root;
-
-	//Experimental and Test Codes//Should delete after
-
-	inline void Log(FString string) { //Print out log (similar to Unity Debug.Log)
-		int test = 123;
-
-		//Takes arguements = (CategoryName, Verbosity, Format, ...)
-		//%s is a place holder for string, %d for int, and %f for float
-		UE_LOG(LogClass, Warning, TEXT("Debug Message here: %s : %d"), *string, test)
-
-			//To find the logs go to (UE): Window >> Developer Tools >> Output Log
-			//Set filter to "LogClass" to easily find the logs
-	}
 protected:
 	// Called when the game starts or when spawned
-	virtual void BeginPlay() override; //Same as Unity's OnStart()
-
-	//Protected variables
-	FVector velocity; //Current speed
-	FVector velocityMax;
-	FVector position;
-	FVector rotation;
-	float mass;
-	float jumpMax;
-	float traction;
-	float handling;
-	int healthMax;
+	virtual void BeginPlay() override;
 
 public:
 	// Called every frame
-	virtual void Tick(float DeltaTime) override; //Same as Unity's Update()
+	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-};
+
+	///Game Codes////////////////////////////////////////////////// 
+
+public:
+	///Components
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UStaticMeshComponent* CarMesh;
+
+	///Variables
+	///Vehicle Attributes
+
+	//The heavier the vehicle the slower it moves and less jumping height
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float mass;
+
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		int healthMax;
+
+	//Not used yet
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		int attack;
+
+	//Not used yet
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		int defense;
+
+	//Drive linear motion
+	//100 kg mass would probably require 10000 force to move
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float force;
+
+	//Drive angular motion
+	//Need about 200 torque to rotate easily
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float torque;
+
+	//Set maximum speed limit
+	//Default is 300.0f
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float speedMax;
+
+	//Value should be between 0 and 1, where 1 means that it gains its max speed in almost one second
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float accelerationRate;
+
+	//The lower the traction the more 'slippery' the movement becomes
+	//Shouldn't be less than 1.0f //Default 50.0f
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float traction;
+
+	//The lower the handling the less grip the vehicle has when turning
+	//Shouldn't be less than 1.0f //Default 50.0f
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float handling;
+
+	//Unused Yet
+	UPROPERTY(EditAnywhere, Category = "Attributes", BlueprintReadWrite)
+		float turningGrip;
+
+	// Keep track of laps completed
+	UPROPERTY(EditAnywhere)
+		uint8 lapCounter;
+
+	//Ground check derive from thrusters
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool isGrounded;
+
+	///Methods
+
+	//Moves vehicle forward/backward
+	UFUNCTION(BlueprintCallable)
+		void Accelerate(float value_);
+
+	//Turn vehicle
+	UFUNCTION(BlueprintCallable)
+		void Turn(float value_);
+
+	//Assign the mesh in the Blueprint by calling this function
+	//Without assigning a Car/Body mesh it can't use the physics from this C++
+	UFUNCTION(BlueprintCallable)
+		void GetCarMesh(class UStaticMeshComponent* CarMesh_);
+
+	UFUNCTION(BlueprintCallable)
+		void HandBrake();
+
+	UFUNCTION(BlueprintCallable)
+		void Jump();
+
+	UFUNCTION(BlueprintCallable)
+		void UseItem();
+
+	UFUNCTION(BlueprintCallable)
+		uint8 GetItemHeld();
+
+	UFUNCTION(BlueprintCallable)
+		void HandleEvents();
+
+	//This method is used to get Scalar value of velocity (e.g. for Speedometer)
+	UFUNCTION(BlueprintCallable)
+		float GetSpeed();
+
+	UFUNCTION(BlueprintCallable)
+		void FallingState();
+
+
+	/// Getters and Setters
+
+	void SetPosition(FVector vec_);
+	FVector GetPosition();
+
+	void SetRotation(FVector vec_);
+	FVector GetRotation();
+
+	//Heal and damage 
+	inline void SetHealth(int value_) {
+		if (health + value_ <= 0) health = 0;
+		if (health + value_ >= healthMax) health = healthMax;
+		else health += value_;
+	}
+	inline int GetHealth() const { return health; }
+
+	inline void SetMaxHealth(int value_) { if (value_ > 0) healthMax = value_; }
+	inline int GetMaxHealth() const { return healthMax; }
+
+	inline void SetMass(float value_) { if (value_ > 0.0f) mass = value_; }
+	inline float GetMass();
+
+	inline void SetAttack(int value_) { attack = value_; }
+	inline int GetAttack() const { return attack; }
+
+	inline void SetDefense(int value_) { defense = value_; }
+	inline int GetDefense() const { return defense; }
+
+	inline void SetTraction(float value_) { traction = value_; }
+	inline float GetTraction() const { return traction; }
+
+	inline void SetHandling(float value_) { handling = value_; }
+	inline float GetHandling() const { return handling; }
+
+	inline void SetAccelerationRate(float value_) { accelerationRate = value_; }
+	inline float GetAccelerationRate() { return accelerationRate; }
+
+private:
+	///Variables
+
+	//Current health 
+	int health;
+
+	//Simple check to calculate gripForce
+	float turningDir;
+
+	//Counters the drifting/slipping effect when turning at high speed
+	float gripForce;
+
+	//Affects residual movement when input = 0
+	float timeElapsedLin;
+
+	//Affects residual rotation when input = 0
+	float timeElapsedRot;
+
+	//Affects acceleration rate
+	float timeElapsedAcc;
+
+	//Affects falling speed
+	float timeElapsedAir;
+
+	//Provides the upper value range of traction
+	float tractionRange;
+
+	//Provides the upper value range of handling
+	float handlingRange;
+
+	FVector position;
+	FVector rotation;
+
+	FVector velLinear;
+	FVector accelLinear;
+
+	FVector velAngular;
+	FVector accelAngular;
+
+	FVector sideForce;
+	FVector jumpForce;
+
+	///Support functionality
+	bool isForwardPressed;
+	bool isTurning;
+	bool jumpState;
+
+	bool isMovingForward();
+	FVector FindLeftVector();
+	FVector FindRightVector();
+
+
+	/// Debug Tools
+
+	// Print out log (similar to Unity's Debug.Log)
+	// To find the logs go to (UE): Window >> Developer Tools >> Output Log
+	inline void LogString(FString string) {
+		//Note: %s = string, %d = int, and %f = float
+		UE_LOG(LogWindows, Warning, TEXT("%s"), *string)
+	}
+
+	inline void LogFloat(float value) {
+		UE_LOG(LogWindows, Warning, TEXT("%f"), value)
+	}
+
+	inline void LogVector(FVector value) {
+		UE_LOG(LogWindows, Warning, TEXT("%f,: %f,: %f"), value.X, value.Y, value.Z)
+	}
+
+	inline void LogRotator(FRotator value) {
+		UE_LOG(LogWindows, Warning, TEXT("%f,: %f,: %f"), value.Roll, value.Pitch, value.Yaw)
+	}
+
+	inline void LogMsg(FString string, float value) {
+		UE_LOG(LogWindows, Warning, TEXT("%s: %f"), *string, value)
+	}
+
+protected:
+
+
+
+};//END OF CLASS
